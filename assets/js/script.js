92cc7cdb749e58a5d72e4deae65687247ba452ff
Create-88 <141834742+Create-88@users.noreply.github.com>
2023-10-02 10:29:02 +1030

    add question objects

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,32 +8,45 @@ window.addEventListener("load", function() {
 });     
 
 //Create variables for what appears at innerHTML
-var questionContent = {
-    question: ["What is the meaning of the method .split?", "What is .include?", "What is .splice?"],
-    choice1: ["Split the function in two.", "Looks for the value within the array. True if present.", "Cuts an array in helf and stores the values in local storage."],
-    choice2: ["Split the array into individual parts or characters.", "Used to push another value into an array", "Cuts a series of values out of the array using the defined parameters."],
-    choice3: ["Split the array into halves.", "Looks for a character within an array", "An icecream you buy from the shop."],
-    answer: ["Split the array into individual parts or characters.", "Looks for the value within the array. True if present.", "Cuts a series of values out of the array using the defined parameters."],
+var question1 = {
+    question: "What is .include?",
+    choice1: "Looks for the value within the array. True if present.",
+    choice2: "Used to push another value into an array",
+    choice3: "Looks for a character within an array", 
+    answer: "Looks for the value within the array. True if present.",
 }
 
-var highScores = [];
+var question2 = {
+    question: "What is the meaning of the method .split?", 
+    choice1: "Split the function in two.", 
+    choice2: "Split the array into individual parts or characters.",
+    choice3: "Split the array into halves.",
+    answer: "Split the array into individual parts or characters.",
+}
+var question3 = {
+    question: "What is .splice?",
+    choice1: "Cuts an array in helf and stores the values in local storage.",
+    choice2: "Cuts a series of values out of the array using the defined parameters.",
+    choice3: "An icecream you buy from the shop.",
+    answer: "Cuts a series of values out of the array using the defined parameters.",
+}
 
 var welcomeScreen = document.getElementById("welcome-screen");
 
 //Variable for startButton
 var startButton = document.getElementById("start-button");
 
-//Write questionContent data to page
+//Write question1 data to page
 var choice1 = document.getElementById("option-1");
 var choice2 = document.getElementById("option-2");
 var choice3 = document.getElementById("option-3");
 var questionHeading = document.getElementById("question-heading");
 var answerTextEl = document.getElementById("check-answer");
-choice1.textContent = questionContent.choice1[0];
-choice2.textContent = questionContent.choice2[0];
-choice3.textContent = questionContent.choice3[0];
-questionHeading.textContent = questionContent.question[0];
-answerTextEl.textContent = questionContent.answer[0];
+choice1.textContent = question1.choice1[0];
+choice2.textContent = question1.choice2[0];
+choice3.textContent = question1.choice3[0];
+questionHeading.textContent = question1.question[0];
+answerTextEl.textContent = question1.answer[0];
 
 
 
@@ -50,16 +63,14 @@ var choiceList = document.getElementById("choice-list");
 //event listener to activate pressing answer - WORKS!
 choiceList.addEventListener("click", function(event) {
     var clickedEl = event.target;
-    if (clickedEl.textContent === questionContent.answer[0]) {
+    if (clickedEl.textContent === question1.answer[0]) {
         answerTextEl.style.visibility = "visible";
-        answerTextEl.textContent = "That's right! " + questionContent.answer[0];
-        console.log(questionContent.question[0]);
-
+        answerTextEl.textContent = "That's right! " + question1.answer[0];
+        console.log(question1.question[0]);
+    } else {
+        answerTextEl.style.visibility = "visible";
+        answerTextEl.textContent = "Study harder or you will fail!";
     }
-    // }) else {
-    //     answerTextEl.style.visibility = "visible";
-    //     answerTextEl.textContent = "Study harder! Try again or you will fail!";
-    // }
 });
 
 
