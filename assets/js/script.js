a5ad27282064da25ca78a5bc05bbae6c6d579962
Create-88 <141834742+Create-88@users.noreply.github.com>
2023-10-02 12:39:16 +1030

    update multiple choice

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,48 +7,44 @@ window.addEventListener("load", function() {
     answerTextEl.style.visibility = "hidden";
 });     
 
-//Create variables for what appears at innerHTML
-var question1 = {
-    question: "What is .include?",
-    choice1: "Looks for the value within the array. True if present.",
-    choice2: "Used to push another value into an array",
-    choice3: "Looks for a character within an array", 
-    answer: "Looks for the value within the array. True if present.",
-}
+//Identifying positions for question data on the page
+var choice1 = document.getElementById("option-1");
+var choice2 = document.getElementById("option-2");
+var choice3 = document.getElementById("option-3");
+var questionHeading = document.getElementById("question-heading");
+var answerTextEl = document.getElementById("check-answer");   
 
-var question2 = {
-    question: "What is the meaning of the method .split?", 
-    choice1: "Split the function in two.", 
-    choice2: "Split the array into individual parts or characters.",
-    choice3: "Split the array into halves.",
-    answer: "Split the array into individual parts or characters.",
-}
-var question3 = {
+//Create variables for what appears 
+var question = {
+    question: "",
+    choice1: "",
+    choice2: "",
+    choice3: "",
+    answer: "",
+};
+
+var set3 = {
     question: "What is .splice?",
     choice1: "Cuts an array in helf and stores the values in local storage.",
     choice2: "Cuts a series of values out of the array using the defined parameters.",
     choice3: "An icecream you buy from the shop.",
-    answer: "Cuts a series of values out of the array using the defined parameters.",
+    answer: "Cuts a series of values out of the array using the defined parameters."
+};
+
+
+function renderQuestion() {
+        choice1.textContent = question.choice1;
+        choice2.textContent = question.choice2;
+        choice3.textContent = question.choice3;
+        questionHeading.textContent = question.question;
+        answerTextEl.textContent = question.answer;
 }
+
 
 var welcomeScreen = document.getElementById("welcome-screen");
 
 //Variable for startButton
 var startButton = document.getElementById("start-button");
-
-//Write question1 data to page
-var choice1 = document.getElementById("option-1");
-var choice2 = document.getElementById("option-2");
-var choice3 = document.getElementById("option-3");
-var questionHeading = document.getElementById("question-heading");
-var answerTextEl = document.getElementById("check-answer");
-choice1.textContent = question1.choice1[0];
-choice2.textContent = question1.choice2[0];
-choice3.textContent = question1.choice3[0];
-questionHeading.textContent = question1.question[0];
-answerTextEl.textContent = question1.answer[0];
-
-
 
 //event listener to activate function WORKS!
 startButton.addEventListener("click", function(event) {
@@ -56,6 +52,12 @@ startButton.addEventListener("click", function(event) {
     startButton.disabled = true;
     welcomeScreen.style.display = "none";
     questionSet.style.visibility = "visible";
+    question.question = "What is .include?",
+    question.choice1 = "Looks for the value within the array. True if present.",
+    question.choice2 = "Used to push another value into an array",
+    question.choice3 = "Looks for a character within an array", 
+    question.answer = "Looks for the value within the array. True if present.",
+    renderQuestion();
 })
 
 var choiceList = document.getElementById("choice-list");
@@ -63,15 +65,24 @@ var choiceList = document.getElementById("choice-list");
 //event listener to activate pressing answer - WORKS!
 choiceList.addEventListener("click", function(event) {
     var clickedEl = event.target;
-    if (clickedEl.textContent === question1.answer[0]) {
+    if (clickedEl.textContent === question.answer) {
         answerTextEl.style.visibility = "visible";
-        answerTextEl.textContent = "That's right! " + question1.answer[0];
-        console.log(question1.question[0]);
+       answerTextEl.textContent = "That's right! " + question.answer;
+        setTimeout(renderQuestion(), 3000);
+        renderQuestion();
+        question.question = "What is the meaning of the method .split?",
+        question.choice1 = "Split the function in two.", 
+        question.choice2 = "Split the array into individual parts or characters.",
+        question.choice3 = "Split the array into halves.",
+        question.answer = "Split the array into individual parts or characters."   
+
     } else {
         answerTextEl.style.visibility = "visible";
         answerTextEl.textContent = "Study harder or you will fail!";
     }
 });
+
+
 
 
 
